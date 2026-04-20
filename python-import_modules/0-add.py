16170592ba8f97e9ec5edda991869608d0be9dae
turgut-bab <13756@holbertonstudents.com>
2026-04-20 16:39:12 +0000

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-def add(a, b):
-    if __name__ == "__main__":
-        a=1
-        b=2
-        print(f"{a}+{b}={add(a, b)}")
-        return (a+b)
+if __name__ == "__main__":
+    a=1
+    b=2
+    print("{}+{}={}".format(a,b,add(a,b))
+    return (a+b)
