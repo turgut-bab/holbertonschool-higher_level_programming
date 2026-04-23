69b289f966ebd42875c27717759bb99a42cb1a9a
turgut-bab <13756@holbertonstudents.com>
2026-04-23 16:34:58 +0000

    safe print list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=10):
+    count = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            except +=1
+        except:
+             break
+    print("")
+    return count
