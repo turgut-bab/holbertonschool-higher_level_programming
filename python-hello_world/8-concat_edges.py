6a339870e88669bd08147c980198d22d56c5b190
turgut-bab <13756@holbertonstudents.com>
2026-04-29 18:52:44 +0000

    concat edges

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = f"{str[39:67]} with {str[:6]}"
+print(str)
