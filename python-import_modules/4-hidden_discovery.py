6029c2ad0f395c2b8057f0827467fa3d0b6be317
turgut-bab <13756@holbertonstudents.com>
2026-05-09 13:29:17 +0000

    hidden discovery

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import hidden_4
+def discover_hidden():
+    all_names = dir(hidden_4)
+    filtered_names = sorted([name for name in all_names if not name.startswith('__')])
+    for name in filtered_names:
+        print(name)
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    for i in names:
-        if not i.startswith("__"):
-            print(i)
+    discover_hidden()
