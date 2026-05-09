592dc28b7156e7ad4f812107afd1c8b98952f560
turgut-bab <13756@holbertonstudents.com>
2026-05-09 14:17:39 +0000

    multiple return

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length > 0:
+        first_char = sentence[0]
+    else:
+        first_char = None
+    return (length, first_char)
