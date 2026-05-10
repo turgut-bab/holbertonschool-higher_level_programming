7295961a19713e08e46215d37867ba5b1c799933
turgut-bab <13756@holbertonstudents.com>
2026-05-10 20:13:08 +0000

    Task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """Prints the first x elements of a list that are integers."""
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+    print("")
+    return count
