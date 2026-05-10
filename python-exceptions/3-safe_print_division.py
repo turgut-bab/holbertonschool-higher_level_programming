b9d71f331f937326a7afbc272c2d256d60d2f3b5
turgut-bab <13756@holbertonstudents.com>
2026-05-10 20:14:17 +0000

    Task 3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """Divides 2 integers and prints the result."""
+    try:
+        result = a / b
+    except (ZeroDivisionError, TypeError):
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
